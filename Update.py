c44df389a4823e3e7d2c4f91607cdd98c67e2e8c
dgbaopro-top1 <73091856+dgbaopro-top1@users.noreply.github.com>
2020-10-26 10:59:21 +0700

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python2
 # coding=utf-8
- os.system("rm -v tools.py")
- os.system("git clone https://github.com/dgbaopro-top1/tools-fbhacked/tools.py")
+os.system("rm -v tools.py")
+os.system("git clone https://github.com/dgbaopro-top1/tools-fbhacked/tools.py")
